2c26b2d85d949024311544fc69797ff4ac1847c9
Michaël Landry <74638031+lethargiesleeps@users.noreply.github.com>
2023-10-11 14:48:21 -0400

    fixed carousel slightly

--- shopping-website/resources/views/components/carousel.blade.php
+++ shopping-website/resources/views/components/carousel.blade.php
@@ -15,8 +15,8 @@
     }
 
     .carousel-indicators .active {
-        background-color: #343a40; /* Bootstrap 5's .bg-warning color */
-        border-color: #343a40; /* Bootstrap 5's .bg-warning color */
+        background-color: white; /* Bootstrap 5's .bg-warning color */
+        /* Bootstrap 5's .bg-warning color */
     }
 
     .carousel-inner .carousel-item img {
@@ -38,7 +38,7 @@
     <div class="carousel-inner text-center">
         <div class="carousel-item active">
             <img src="/images/place-holder.png" class="mx-auto d-block" alt="...">
-            <div class="carousel-caption d-flex h-100 align-items-center justify-content-center">
+            <div class="carousel-caption d-flex align-items-center justify-content-center" style="background-color: #343a40; color: white;">
                 <div>
                     <h5>NEW KITCHEN ITEMS</h5>
                     <p>Check out our new collection of things for your kitchen!</p>
@@ -47,7 +47,7 @@
         </div>
         <div class="carousel-item">
             <img src="/images/place-holder.png" class="mx-auto d-block" alt="...">
-            <div class="carousel-caption d-flex h-100 align-items-center justify-content-center">
+            <div class="carousel-caption d-flex h-25 align-items-center justify-content-center" style="background-color: #343a40; color: white;">
                 <div>
                     <h5>15% OFF WOMEN'S WINTER WEAR</h5>
                     <p>Don't miss out on this flash sale!</p>
@@ -56,7 +56,7 @@
         </div>
         <div class="carousel-item">
             <img src="/images/place-holder.png" class="mx-auto d-block" alt="...">
-            <div class="carousel-caption d-flex h-100 align-items-center justify-content-center">
+            <div class="carousel-caption d-flex h-25 align-items-center justify-content-center" style="background-color: #343a40; color: white;">
                 <div>
                     <h5>FREE SHIPPING ON ORDERS ABOVE $300</h5>
                     <p>Buy in bulk and save!</p>
